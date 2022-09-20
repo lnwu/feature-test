8f8383a75d21893912d622cb6509dc9a8cd10f46
lnwu <wind2729@gmail.com>
2022-09-20 17:23:51 +0800

    add qps test

---
+++ packages/web-qps-test/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 1,
+      limit: 60,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+})
+export class AppModule {}
